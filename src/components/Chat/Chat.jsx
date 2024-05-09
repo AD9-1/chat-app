4b33bb320034835899bdd936209c818986d1c2b7
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-05-09 03:10:39 -0700

    css files

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
@@ -7,7 +7,21 @@ import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 import LinkedCameraIcon from "@mui/icons-material/LinkedCamera";
 import MicIcon from "@mui/icons-material/Mic";
 import "./Chat.css";
+import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
+  const [emoji, setEmoji] = useState(false);
+  const [text, setText] = useState();
+  const endRef = useRef(null);
+  useEffect(() => {
+    console.log(endRef)
+    endRef.current.scrollIntoView({behavior:"smooth"});
+  }, []);
+  const handleClick = (e) => {
+    console.log(e);
+    setText((prev) => prev + e.emoji);
+    setEmoji(false);
+    console.log(text);
+  };
   return (
     <div className="chat">
       <div className="top">
@@ -24,17 +38,88 @@ const Chat = () => {
           <InfoOutlinedIcon style={{ fontSize: "25px" }} />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="text">
+            {" "}
+            <p>
+              t looks like you're trying to handle a click event and update some
+              text. However, there's a syntax error in your code. The setText
+              function is not properly updating the text state. Here's the
+              corrected version:
+            </p>
+            <h4>1 min ago</h4>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="text">
+            {" "}
+            <p>
+              t looks like you're trying to handle a click event and update some
+              text. However, there's a syntax error in your code. The setText
+              function is not properly updating the text state. You're using an
+              arrow function but not returning anything from it. Here's the
+              corrected version:
+            </p>
+            <h4>1 min ago</h4>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="text">
+            {" "}
+            <p>
+              t looks like you're trying to handle a click event and update some
+              text. However, there's a syntax error in your code. The setText
+              function is not properly updating the text state. You're using an
+              arrow function but not returning anything from it. Here's the
+              corrected version:
+            </p>
+            <h4>1 min ago</h4>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="text">
+            <img src="download.jpg" alt="" />
+            <p>
+              t looks like you're trying to handle a click event and update some
+              text. However, there's a syntax error in your code. The setText
+              function is not properly updating the text state. You're using an
+              arrow function but not returning anything from it. Here's the
+              corrected version:
+            </p>
+            <h4>1 min ago</h4>
+          </div>
+          <div ref={endRef}></div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
-          <InsertPhotoIcon style={{ fontSize: "20px" }}/>
+          <InsertPhotoIcon style={{ fontSize: "20px" }} />
           <LinkedCameraIcon style={{ fontSize: "20px" }} />
           <MicIcon style={{ fontSize: "23px" }} />
         </div>
-        <input type="text" placeholder="Type a text..." />
+        <input
+          type="text"
+          placeholder="Type a text..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className="emoji">
-          <Face4OutlinedIcon style={{ fontSize: "20px" }}/>
+          <Face4OutlinedIcon
+            style={{ fontSize: "20px", color: "yellow" }}
+            onClick={() => setEmoji((prev) => !prev)}
+          />
+          <div className="picker">
+            {" "}
+            <EmojiPicker open={emoji} onEmojiClick={handleClick} />
+          </div>
         </div>
+
         <button type="submit" className="sendbutton">
           Send
         </button>
--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
-import "./Detail.css"
+import React from "react";
+import "./Detail.css";
+import KeyboardArrowUpRoundedIcon from "@mui/icons-material/KeyboardArrowUpRounded";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import DownloadingRoundedIcon from "@mui/icons-material/DownloadingRounded";
 const Detail = () => {
   return (
-    <div className='detail'>
-      Detail
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum doe</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <KeyboardArrowUpRoundedIcon />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <KeyboardArrowUpRoundedIcon />
+          </div>
+        </div>
+        <div className="option photoes">
+          <div className="title">
+            <span>Shared photos</span>
+            <KeyboardArrowDownRoundedIcon />
+          </div>
+          <div className="photo">
+            <div className="photoitem">
+              <div className="photodetail">
+                <img src="download.jpg" alt="" />
+                <span>Photo33344211</span>
+              </div>
+              <DownloadingRoundedIcon className="downloadicon" />
+            </div>
+            <div className="photoitem">
+              <div className="photodetail">
+                <img src="download.jpg" alt="" />
+                <span>Photo33344211</span>
+              </div>
+              <DownloadingRoundedIcon className="downloadicon" />
+            </div>
+            <div className="photoitem">
+              <div className="photodetail">
+                <img src="download.jpg" alt="" />
+                <span>Photo33344211</span>
+              </div>
+              <DownloadingRoundedIcon className="downloadicon" />
+            </div>
+            <div className="photoitem">
+              <div className="photodetail">
+                <img src="download.jpg" alt="" />
+                <span>Photo33344211</span>
+              </div>
+              <DownloadingRoundedIcon className="downloadicon"  />
+            </div>
+            <div className="photoitem">
+              <div className="photodetail">
+                <img src="download.jpg" alt="" />
+                <span>Photo33344211</span>
+              </div>
+              <DownloadingRoundedIcon className="downloadicon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <KeyboardArrowUpRoundedIcon />
+          </div>
+        </div>
+      </div>
+      <button>Block User</button>
+      <button className="logout">Logout</button>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
