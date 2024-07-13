865f6718080e148a517b2fdb94fb0c389faa56e2
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-12 18:50:03 -0700

    components built up

--- src/components/Chatlist/AddUser/AddUser.jsx
+++ src/components/Chatlist/AddUser/AddUser.jsx
@@ -1,39 +1,67 @@
 import React, { useState } from "react";
 import "./AddUser.css";
 import { db } from "../../../lib/firebase";
-import { collection, getDoc, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  where,
+} from "firebase/firestore";
 
 const AddUser = () => {
-  const[user,setUser]=useState(null)
-  const handleSubmit = async (e) => {
+  const [users, setUsers] = useState(null);
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    const chatRef = collection(db, "chats");
+    const userChatsRef = collection(chatRef, "userchats");
+    try {
+      const newChatRef = doc(chatRef);
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        messages: [],
+      });
+      console.log(newChatRef.id);
+    } catch (err) {}
+  };
+
+  const handleSearch = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const { username } = Object.formEntries(formData);
+    const { username } = Object.fromEntries(formData);
+    // const username  = formData.get("username")
+
     try {
       const userRef = collection(db, "users");
       const q = query(userRef, where("username", "==", username));
-      const quesrySnapShot=await getDoc(q);
-if(!quesrySnapShot.empty)
-  {
-
-  }
+      const querySnapShot = await getDocs(q);
+      console.log(querySnapShot.docs[0].data());
+      if (!querySnapShot.empty) {
+        setUsers(querySnapShot.docs.map((doc) => doc.data()));
+      }
     } catch (err) {
-      console.log(err.message);
+      console.log(err);
     }
   };
   return (
     <div className="adduser">
-      <form action="" onSubmit={handleSubmit}>
+      <form action="" onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="details">
-          <img src="./avatar.png" alt="" />
-          <p>Jane Doe</p>
-        </div>
-        <button>Add User</button>
-      </div>
+
+      {users &&
+        users.map((user) => (
+          <div className="user">
+            <div className="details">
+              <img src={user.avatar || "./avatar.png"} alt="" />
+              <p>{user.username}</p>
+            </div>
+            <button onClick={handleAdd}>Add User</button>
+          </div>
+        ))}
     </div>
   );
 };
